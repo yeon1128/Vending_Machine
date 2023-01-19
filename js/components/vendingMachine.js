aed83ed56b67ea5acb62e5250448a5d88fc6f2a5
jeongyeon.seo <s9530348@naver.com>
2023-01-19 14:48:34 +0900

    feat: 자판기 음료 메뉴 클릭하면 음료 목록 생성

--- js/components/vendingMachine.js
+++ js/components/vendingMachine.js
@@ -2,8 +2,67 @@ class VendingMachine {
   constructor() {
     const vendingMachine = document.querySelector(".vending-machine");
     this.itemList = vendingMachine.querySelector(".list-drink");
+    this.stagedList = vendingMachine.querySelector(".list-staged");
+    this.balance = vendingMachine.querySelector(".num-balance");
   }
+
+  stagedItemGenerator(target) {
+    const stagedItem = document.createElement("li");
+    stagedItem.dataset.item = target.dataset.item;
+    stagedItem.dataset.price = target.dataset.price;
+    console.log(target.dataset.img);
+
+    stagedItem.innerHTML = `
+    <button type="button" class="list-staged-item">
+      <img class="img-drink" src="./images/${target.dataset.img}" alt="" />
+      <strong class="txt-drink">${target.dataset.item}</strong>
+      <span class="count-drink">1</span>
+    </button>
+    `;
+    this.stagedList.appendChild(stagedItem);
+  }
+
   setup() {
     this.bindEvents();
   }
+
+  bindEvents() {
+    const btnsCola = this.itemList.querySelectorAll("button");
+
+    btnsCola.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        const targetEl = e.currentTarget;
+        const targetElPrice = parseInt(targetEl.dataset.price);
+        const stagedListItem = this.stagedList.querySelectorAll("li");
+        const balanceVal = parseInt(
+          this.balance.textContent.replaceAll(",", "")
+        );
+        let isStaged = false;
+
+        console.log(targetEl);
+        if (balanceVal >= targetElPrice) {
+          this.balance.textContent =
+            new Intl.NumberFormat().format(balanceVal - targetElPrice) + "원";
+
+          for (const item of stagedListItem) {
+            if (item.dataset.item === targetEl.dataset.item) {
+              item.querySelector(".count-drink").textContent++;
+              isStaged = true;
+              break;
+            }
+          }
+
+          if (!isStaged) {
+            this.stagedItemGenerator(targetEl);
+          }
+
+          targetEl.dataset.count--;
+        } else {
+          alert("잔액 부족");
+        }
+      });
+    });
+  }
 }
+
+export default VendingMachine;
--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import ColaGenerator from "./components/colaGenerator.js";
+import Vendingmachine from "./components/vendingMachine.js";
 
 const colaGenerator = new ColaGenerator();
+const vendingMachine = new Vendingmachine();
 
-colaGenerator.setup();
-// colaGenerator.colaFatory();
+await colaGenerator.setup();
+vendingMachine.setup();
